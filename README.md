7176541e4ce509c5bf4133dd5ef05f7f42e3ffd0
zyc-816 <860816655@qq.com>
2025-11-30 21:51:27 +0800

    1.0.0 初步完成

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# python-project
-a python course design on Time Series Forecasting
+# 《Python时序预测综合实践》大作业 
+---
+## 运行方法  
+- 输入命令`pip install -r requirements.txt`安装依赖
+- 运行main.py
+## 说明
+- 生成的图表位于./tables
+- 数据文件位于./data
+- 终端输出为三种模型的误差指标
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from scipy import stats
 import statsmodels.api as sm
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import warnings
+warnings.filterwarnings('ignore')
 
 # 读取数据
 data = pd.read_csv("./data/train.csv")
@@ -24,14 +28,27 @@ z_scores = stats.zscore(sales_summary["sales"])
 outliters = (abs(z_scores) > 2).astype(int)
 df_clean = sales_summary[outliters == 0]
 data = df_clean
+data.loc[:, 'date'] = pd.to_datetime(data['date'])
+
+# 绘制原始折线图
+plt.style.use('dark_background')
+plt.figure(figsize=(18, 6))
+plt.plot(data["date"], data["sales"], label='origin', color='#1f77b4')
+plt.title('Origin Data')
+plt.xlabel('Date')
+plt.ylabel('Sales')
+plt.legend()
+# 保存图表
+plt.savefig("./tables/origin.png", dpi = 400)
 
 # 划分训练集，测试集
 train_data = data.iloc[:int(0.8*len(data)), :]
-train_data['date'] = pd.to_datetime(train_data['date'])
+train_data.loc[:, 'date'] = pd.to_datetime(train_data['date'])
 train_data.set_index('date', inplace = True)
 test_data = data.iloc[int(0.8*len(data)):, :]
-test_data['date'] = pd.to_datetime(test_data['date'])
+test_data.loc[:, 'date'] = pd.to_datetime(test_data['date'])
 test_data.set_index('date', inplace = True)
+
 # 定义Holt-Winters模型
 model_1 = sm.tsa.ExponentialSmoothing(
     train_data["sales"],
@@ -41,9 +58,16 @@ model_1 = sm.tsa.ExponentialSmoothing(
 ).fit(
     smoothing_level=0.5,
 )
-forecast_1 = model_1.forecast(steps=len(test_data)).round(1)
+forecast_1 = model_1.forecast(steps=len(test_data))
 forecast_index_1 = test_data.index
 forecast_1.index = test_data.index
+
+mae_1 = mean_absolute_error(test_data, forecast_1)
+rmse_1 = np.sqrt(mean_squared_error(test_data, forecast_1)).round(1)
+test_sales = test_data["sales"]
+non_zero = test_sales != 0
+mape_1 = np.mean(np.abs((test_sales[non_zero] - forecast_1[non_zero]) / test_sales[non_zero])) * 100
+print(f"Holt-Winters:\n\tMAE = {round(mae_1,1)}\n\tRMSE = {round(rmse_1,1)}\n\tMAPE = {round(mape_1,1)}%\n")
 
 # 定义SARIMA模型
 p, d, q = 1, 1, 1
@@ -52,6 +76,11 @@ model_2 = SARIMAX(train_data["sales"], order=(p, d, q), seasonal_order=(P, D, Q,
 forecast_2 = model_2.forecast(steps=len(test_data)).round(1)
 forecast_index_2 = test_data.index
 forecast_2.index = test_data.index
+
+mae_2 = mean_absolute_error(test_data, forecast_2)
+rmse_2 = np.sqrt(mean_squared_error(test_data, forecast_2)).round(1)
+mape_2 = np.mean(np.abs((test_sales[non_zero] - forecast_2[non_zero]) / test_sales[non_zero])) * 100
+print(f"Holt-Winters:\n\tMAE = {round(mae_2,1)}\n\tRMSE = {round(rmse_2,1)}\n\tMAPE = {round(mape_2,1)}%\n")
 
 # 定义随机森林模型
 _train_data = train_data.copy()
@@ -78,8 +107,12 @@ forecast_3 = pd.DataFrame({
 })
 forecast_3['date'] = pd.to_datetime(forecast_3['date'])
 forecast_3.set_index('date', inplace = True)
+mae_3 = mean_absolute_error(test_data, forecast_3)
+rmse_3 = np.sqrt(mean_squared_error(test_data, forecast_3)).round(1)
+sales_3 = forecast_3["sales"]
+mape_3 = np.mean(np.abs((test_sales[non_zero] - sales_3[non_zero]) / test_sales[non_zero])) * 100
+print(f"Holt-Winters:\n\tMAE = {round(mae_3,1)}\n\tRMSE = {round(rmse_3,1)}\n\tMAPE = {round(mape_3,1)}%\n")
 
-plt.style.use('dark_background')
 plt.figure(figsize=(18, 6))
 # 绘制测试集的数据
 plt.plot(test_data.index, test_data, label='Actual', color='#1f77b4')
